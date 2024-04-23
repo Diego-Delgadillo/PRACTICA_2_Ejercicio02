5d9a67c5c12d01d840d265d3e65b13d4016cd96a
Diego-Delgadillo <drdg2005@gmail.com>
2024-04-22 21:06:59 -0600

    Versión preliminar donde sólo falta agrgar la función de gnera correctamente el archivo .txt

--- PRACTICA_2_Ejercicio02/Program.cs
+++ PRACTICA_2_Ejercicio02/Program.cs
@@ -9,10 +9,11 @@ los elementos, mostrarlos y guardar todos los elementos almacenados hasta el mom
 debe revisar que elementos se encuentran ya guardados)
 */
 
-
 using System;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Xml.Schema; // librería utilizada para ir generando loa archivos .txt
 
 //Declaramos la clase para los elementos
 public class elemento
@@ -24,32 +25,46 @@ public class elemento
     public double[] masaAtomica;
     public double[] radioAtomico;
     public string[] estadoOrdinarioElemento;
+
+    //este variable nos permite ir ingresando datos y mantiene el registro de la última posicon
+    private int ultimaPosicion = 0;
     
 
     //ahora creamos un método para ingresar cada atributo dentro de un arreglo
     public void ingresarDatos(int nElementos)
     {
-        nombreElemento = new string[nElementos];
-        simboloElemento = new string[nElementos];
-        noAtomicoElemento = new int[nElementos];
-        masaAtomica = new double[nElementos];
-        radioAtomico = new double[nElementos];
-        estadoOrdinarioElemento = new string[nElementos];
-
-
-        //creamos un ciclo for para ir almacenando cada atributo en el arreglo
-        for (int i = 0; i < nElementos; i++)
+        if (nombreElemento == null)
         {
-            string c; bool num; 
-            Console.WriteLine("\n");
-            Console.WriteLine("Ingrese el nombre del elemento {0}: ",i + 1);
-            nombreElemento[i] = Console.ReadLine();
-            Console.WriteLine("Ingrese el símbolo del elemento {0}: ",i + 1);
-            simboloElemento[i] = Console.ReadLine();
+            nombreElemento = new string[nElementos];
+            simboloElemento = new string[nElementos];
+            noAtomicoElemento = new int[nElementos];
+            masaAtomica = new double[nElementos];
+            radioAtomico = new double[nElementos];
+            estadoOrdinarioElemento = new string[nElementos];
+        }
+
+        else
+        {
+            // Si ya hay datos en los arreglos, ampliamos su tamaño
+            Array.Resize(ref nombreElemento, ultimaPosicion + nElementos);
+            Array.Resize(ref simboloElemento, ultimaPosicion + nElementos);
+            Array.Resize(ref noAtomicoElemento, ultimaPosicion + nElementos);
+            Array.Resize(ref masaAtomica, ultimaPosicion + nElementos);
+            Array.Resize(ref radioAtomico, ultimaPosicion + nElementos);
+            Array.Resize(ref estadoOrdinarioElemento, ultimaPosicion + nElementos);
+        }
+
+     
+        //creamos un ciclo for para ir almacenando cada atributo en el arreglo
+        for (int i = ultimaPosicion; i < ultimaPosicion + nElementos; i++)
+        {
+            string c; bool num;
+            Console.WriteLine("\nIngrese el nombre del elemento {0}: ", i+ 1); nombreElemento[i] = Console.ReadLine();
+            Console.WriteLine("Ingrese el símbolo del elemento {0}: ", i + 1);simboloElemento[i] = Console.ReadLine();
 
             do// este ciclo es para que se ingresen solo números enteros mayores a cero
             {
-                Console.WriteLine("Ingrese el número atómico del elemento {0}: ", i + 1);
+                Console.WriteLine("Ingrese el número atómico del elemento {0}: ", i+ 1);
                 c = Console.ReadLine();
                 num = int.TryParse(c, out noAtomicoElemento[i]);
             } while (!num || noAtomicoElemento[i] <= 0);
@@ -65,31 +80,49 @@ public class elemento
 
             do // este ciclo es para que se ingresen solo números flotantes mayores a cero
             {
-                Console.WriteLine("Ingrese el radio atómico del elemento {0}: ", i + 1);
-                c= Console.ReadLine();
+                Console.WriteLine("Ingrese el radio atómico del elemento {0}: ", i+ 1);
+                c = Console.ReadLine();
                 num = double.TryParse(c, out radioAtomico[i]);
             } while (!num || radioAtomico[i] <= 0);
-            Console.WriteLine("Ingrese el radio atómico del elemento {0}: ",i + 1);
             radioAtomico[i] = Convert.ToDouble(c);
 
 
-            Console.WriteLine("Ingrese el estado ordinario del elemento {0}: ",i + 1);
+            Console.WriteLine("Ingrese el estado ordinario del elemento {0}: ", i + 1);
             estadoOrdinarioElemento[i] = Console.ReadLine();
         }
+        ultimaPosicion += nElementos;
     }
 
-    public void imprimirDatos(int nElementos) //este método nos ayuda a imprimir los datos ingreesados previamente
+    public void imprimirDatos() //este método nos ayuda a imprimir los datos ingreesados previamente
     {
 
         Console.WriteLine("\n");
-        Console.WriteLine("Nombre\tSímbolo\tNo.atómico\t Masa atómica\t Estado ordinario\t");
+        Console.WriteLine("Nombre\tSímbolo\tNo.atómico\t Masa atómica\t Radio atómico\t Estado ordinario\t");
         Console.WriteLine("\n");
         //Ciclo for que imprime los elementos
-        for (int j = 0; j < nElementos; j++)
+        for (int j = 0; j < ultimaPosicion; j++)
         {
             Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", nombreElemento[j], simboloElemento[j], noAtomicoElemento[j], masaAtomica[j], radioAtomico[j], estadoOrdinarioElemento[j]);
             Console.WriteLine("\n");
         }
+    }
+
+    //este es el metodo para guardar los datos en un archivo .txt
+    public void guardarDatos(int nElementos)
+    {
+        Console.WriteLine("\nArchivo generado exitosamente!");
+        Console.WriteLine("Revise la ruta de este programa para abrir el archivo .txt\n");
+        Console.Beep();
+        
+
+
+        for (int i = 0; i < nElementos; i++)
+        {   
+            File.AppendAllText("Elementos.txt", nombreElemento[i]);
+
+                    
+        }
+
     }
 
 }
@@ -124,16 +157,17 @@ public class program()
             return opcionMenu;
         }
 
-        while(opcionMenu == true)
+        while (opcionMenu == true)
         {
+
 
             Console.WriteLine("Este programa permite ingresar, guardar y mostrar datos de distintos elementos químicos.\n");
 
 
             do
             {
-                Console.WriteLine("Ingrese una opción: (1)Ingresar datos, (2)Mostrar datos, (3)Almacenar en un archivo(.txt), (4)Salir\r\n");
-                cadena = Console.ReadLine();    
+                Console.WriteLine("Ingrese una opción: (1)Ingresar datos, (2)Mostrar datos, (3)Almacenar en un archivo(.txt), (4)Salir\n");
+                cadena = Console.ReadLine();
                 num = int.TryParse(cadena, out opcionMenu1);
             } while (!num);
 
@@ -150,11 +184,14 @@ public class program()
                     break;
 
                 case 2:
-                    elementosIngresados.imprimirDatos(n);
+                    elementosIngresados.imprimirDatos();
                     opcionMenu = continuar();
                     break;
 
                 case 3: //esta opcion genera un archivo .txt con los datos posteriormente se ira desarrollando esta parte
+
+
+                    elementosIngresados.guardarDatos(n);
                     opcionMenu = continuar();
                     break;
 
@@ -167,8 +204,8 @@ public class program()
         }
 
         Console.WriteLine("\nPrograma terminado!\nPresione cualquier tecla para continuar...");
-        Console.ReadKey();Console.Beep();
-        
+        Console.ReadKey(); Console.Beep();
+
     }
 }
 
@@ -177,4 +214,4 @@ public class program()
 
 
 
- 
+
